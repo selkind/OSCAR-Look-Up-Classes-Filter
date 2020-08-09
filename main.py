8002beaaddfd7aa9604051968213c01295261be4
Sam Elkind <samuel.elkind@gmail.com>
2020-08-09 15:13:55 -0400

    configure basic logging

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ import argparse
 import os
 import pandas as pd
 import pprint as pp
+import logging
 
 def main(file_path):
     pass
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s\n%(levelname)s: %(message)s", 
+        datefmt="%Y/%m/%d %I:%M%S %p", level=logging.INFO)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath", 
+    parser.add_argument(
+        "filepath", 
         nargs=1, 
         type=str, 
         metavar="path/to/html/source",
         help="The absolute path to the html file containing the courses searched")
     args = parser.parse_args()
 
-    print(args.filepath[0])
+    logging.info(f"Attempting to parse file: {args.filepath[0]}")
+    main(args)
 
