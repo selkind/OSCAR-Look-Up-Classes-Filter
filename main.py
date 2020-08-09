dfe522f0f404e578969d209a42c73c1764052336
Sam Elkind <samuel.elkind@gmail.com>
2020-08-09 16:11:27 -0400

    complete filtering, save raw table to file path const

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import logging
 
 contents_key = 5
 table_top_level_index = "Computational Science & Engr"
+desired_subjects = ["CSE", "CS", "ISYE", "PUBP"]
+un_desired_sections = ["OAN", "OCY", "OCL", "OAH"]
+numeric_col_names = ["CRN", "Crse", "Cred", "Cap", "Act", "Rem", "WL Cap", "WL Act", "WL Rem"]
+useful_cols = ["CRN", "Subj", "Crse", "Sec", "Cred", "Title", "Cap", "Act", "Rem", "WL Cap", "WL Act", "WL Rem", "Instructor"]
+SORT_COLUMNS = ["Subj", "WL Act"]
+OUT_PATH = "/mnt/c/Users/samue/Desktop/filtered_table.html"
 
 def main(file_path):
     with open(file_path, 'r') as f:
@@ -14,18 +20,29 @@ def main(file_path):
     logging.info("Raw data loaded")
 
     table_data = data[contents_key][table_top_level_index]
-
     logging.debug(table_data["Subj"].unique())
 
-    subj_filtered_data = table_data[(table_data["Subj"] == "CSE") | (table_data["Subj"] == "CS") | (table_data["Subj"] == "ISYE") | (table_data["Subj"] == "PUBP")]
-
+    subj_filtered_data = table_data[table_data["Subj"].isin(desired_subjects)]
+    logging.debug(subj_filtered_data["Subj"].unique())
     logging.debug(subj_filtered_data["Sec"].unique())
 
+    section_filtered_data = subj_filtered_data[~subj_filtered_data["Sec"].isin(un_desired_sections)]
+
+    logging.debug(section_filtered_data["Sec"].unique())
+
+    section_filtered_data[numeric_col_names] = section_filtered_data[numeric_col_names].apply(pd.to_numeric, errors="ignore")
+
+    useful_data = section_filtered_data.sort_values(SORT_COLUMNS)[useful_cols]
+
+    with open(OUT_PATH, 'w+') as f_out:
+        f_out.write(useful_data.to_html())
+
+    logging.info(f"Filtered table written to {OUT_PATH}")
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s\n%(levelname)s: %(message)s\n", 
-        datefmt="%Y/%m/%d %I:%M:%S %p", level=logging.DEBUG)
+        datefmt="%Y/%m/%d %I:%M:%S %p", level=logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
