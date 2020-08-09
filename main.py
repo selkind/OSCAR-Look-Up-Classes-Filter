ea96e301fa46708ab89a2df96ee3b2b6730dbc22
Sam Elkind <samuel.elkind@gmail.com>
2020-08-09 15:21:11 -0400

    load data, filter out irrelevant tables, test by logging subject uniques

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import sys
 import argparse
-import os
 import pandas as pd
 import pprint as pp
 import logging
 
+contents_key = 5
+table_top_level_index = "Computational Science & Engr"
+
 def main(file_path):
-    pass
+    with open(file_path, 'r') as f:
+        data = pd.read_html(f)
+    logging.info("Raw data loaded")
+
+    table_data = data[contents_key][table_top_level_index]
+    logging.debug(table_data["Subj"].unique())
+
+
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s\n%(levelname)s: %(message)s", 
-        datefmt="%Y/%m/%d %I:%M%S %p", level=logging.INFO)
+        datefmt="%Y/%m/%d %I:%M%S %p", level=logging.DEBUG)
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "filepath", 
+        "file_path", 
         nargs=1, 
         type=str, 
         metavar="path/to/html/source",
         help="The absolute path to the html file containing the courses searched")
     args = parser.parse_args()
 
-    logging.info(f"Attempting to parse file: {args.filepath[0]}")
-    main(args)
+    logging.info(f"Attempting to parse file: {args.file_path[0]}")
+    main(args.file_path[0])
 
